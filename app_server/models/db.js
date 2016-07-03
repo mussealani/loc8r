20a272a671ecd743508afd2628e76399949ab4a3
mussealani <mohtof2000@gmail.com>
2016-07-04 00:26:55 +0200

    connect to remote database

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,54 +1,49 @@
-var mongoose = require( 'mongoose' ),
-		dbURI = 'mongodb://localhost/Loc8r';
-
-// check if producton-env in use
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
-	// production-env database (we can use this URI)
-	
-	//dbURI = 'mongodb://admin:loc8rNode@ds011745.mlab.com:11745/loc8r-mrashid';
-	
-	// OR
-	dbURI = process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGOLAB_URI;
 }
+
 mongoose.connect(dbURI);
 
-mongoose.connection.on('connected', function () {
-	 console.log('Mongoose connected to ' + dbURI);
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
 });
 
-mongoose.connection.on('error', function (err) {
-	 console.log('Mongoose connection error ' + err); 
-});
-
-mongoose.connection.on('disconnected', function () {
-	 console.log('Mongoose disconnected');
-});
-
-// disconnecting mongodb when the application stops
-var gracefulShutdown = function (msg, callback) {
-	 mongoose.connection.close(function () {
-	 	 console.log('Mongoose disconnected through ' + msg);
-	 	 callback();
-	 });
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
 };
-
-process.once('SIGUSR2', function () {
-	 gracefulShutdown('nodemon restart', function () {
-	 	 process.kill(process.pid, 'SIGUSR2');
-	 });
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
 
-process.on('SIGINT', function () {
-	 gracefulShutdown('app terminating', function () {
-	 	 process.exit(0);
-	 });
-});
-
-process.on('SIGTERM', function () {
-	 gracefulShutdown('Heroku app shutdown', function () {
-	 	 process.exit(0);
-	 });
-});
-
-// require locations.js
+// BRING IN YOUR SCHEMAS & MODELS
 require('./locations');
